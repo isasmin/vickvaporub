0faba0a82036173fd0a153ac1bfe514021b06514
isasmin <liroyasmin@gmail.com>
2025-04-30 09:52:49 -0300

    arrumando venda

--- codigo/funcoes.php
+++ codigo/funcoes.php
@@ -124,17 +124,21 @@ function salvarUsuario($conexao, $nome, $email, $senha) {
     return $funcionou;
 };
  
-// function salvarVenda($conexao, $idcliente, $idproduto, $valor_total, $data) {
-    // $sql = "INSERT INTO tb_venda (idcliente, idproduto, valor_total, data) VALUES (?, ?, ?, ?)";
-    // $comando = mysqli_prepare($conexao, $sql);
+function salvarVenda($conexao, $idcliente, $valor_total, $data) {
+    $sql = "INSERT INTO tb_venda (idcliente, valor_total, data) VALUES (?, ?, ?)";
+    $comando = mysqli_prepare($conexao, $sql);
 
-    // mysqli_stmt_bind_param($comando, 'iids', $idcliente, $idproduto, $valor_total, $data);
+    mysqli_stmt_bind_param($comando, 'ids', $idcliente, $valor_total, $data);
 
-    // $funcionou = mysqli_stmt_execute($comando);
-    // mysqli_stmt_close($comando);
+    $funcionou = mysqli_stmt_execute($comando);
+
+    // retorna o valor do id que acabou de ser inserido
+    $idvenda = mysqli_stmt_insert_id($comando);
     
-    // return $funcionou;
-// };
+    mysqli_stmt_close($comando);
+    
+    return $idvenda;
+};
 
 function salvarItemVenda($conexao, $id_venda, $id_produto, $quantidade) {
     $sql = "INSERT INTO tb_item_venda (idvenda, idproduto, quantidade) VALUES (?, ?, ?)";
--- codigo/tests/teste_salvarVenda.php
+++ codigo/tests/teste_salvarVenda.php
@@ -1,11 +1,12 @@
 <?php
-    require_once "../conexao.php";
-    require_once "../funcoes.php";
+require_once "../conexao.php";
+require_once "../funcoes.php";
 
-    $idcliente = 1; //existe o cliente com id 3?
-    $idproduto = 1; //existe o produto com id 1?
-    $valor_total = 25.50;
-    $data = "2025-03-27";
+$idcliente = 1;
+$valor_total = 25.50;
+$data = "2025-03-27";
 
-    salvarVenda($conexao, $idcliente, $idproduto, $valor_total, $data);
+$idvenda = salvarVenda($conexao, $idcliente, $valor_total, $data);
+
+echo $idvenda;
 ?>
