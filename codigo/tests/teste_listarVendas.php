889833d8217a6a34402e77f29c44bb09212b15fd
yasmin <liroyasmin@gmail.com>
2025-04-30 16:05:51 -0300

    Update teste_listarVendas.php

--- codigo/tests/teste_listarVendas.php
+++ codigo/tests/teste_listarVendas.php
@@ -3,14 +3,7 @@ require_once "../conexao.php";
 require_once "../funcoes.php";
 
 echo "<pre>";
-$vendas = listarVendas($conexao);
-
-foreach ($vendas as $venda) {
-    echo "id venda: {$venda['idvenda']}<br>";
-    echo "cliente: {$venda['nome_cliente']}<br>";
-    // echo "produto: {$venda['nome_produto']}<br>";
-    echo "data da venda: {$venda['data']}<br>";
-}
+print_r(listarVendas($conexao));
 echo "</pre>";
-?>
 
+?>
