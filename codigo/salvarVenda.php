ff81a34e1ea9034a052b1f8fa16ed6773af4976b
isasmin <liroyasmin@gmail.com>
2025-06-11 08:46:10 -0300

    arrumando salvar vendqa

--- codigo/salvarVenda.php
+++ codigo/salvarVenda.php
@@ -1,9 +1,22 @@
 <?php
+require_once "../funcoes.php";
+require_once "../conexao.php";
 
-$idcliente = $_GET['idcliente'];
-$valor = $_GET['valor_total'];
-$data = $_GET['data_comrpa'];
-$idproduto = $_GET['idproduto'];
-$
+$idcliente = $_POST["idcliente"];
+$valor_total = $_POST["valor"];
+$data = $_POST["data"];
+$produtos = $_POST["produto"];
+$id_venda = salvarVenda($conexao, $idcliente, $valor_total, $data);
 
-?>
+for ($i = 0; $i < count($produtos); $i++) {
+    $id_produto = $produtos[$i][0];
+    $quantidade = $produtos[$i][1];
+
+    if ($id_produto && $quantidade) {
+        salvarItemVenda($conexao, $id_venda, $id_produto, $quantidade);
+    }
+}
+
+echo "<h3>Itens da venda cadastrados:</h3><pre>";
+print_r(listarItemVenda($conexao));
+echo "</pre>";
